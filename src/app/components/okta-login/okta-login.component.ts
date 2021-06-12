ec444a5173bd0626b3d406b9c9fb6fe299124998
Gabriel Bravo <>
2021-06-12 09:55:52 -0400

    security added

---
+++ src/app/components/okta-login/okta-login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+import * as OktaSignIn from '@okta/okta-signin-widget';
+
+import myAppConfig from '../../config/my-app-config';
+
+@Component({
+  selector: 'app-okta-login',
+  templateUrl: './okta-login.component.html',
+  styleUrls: ['./okta-login.component.scss']
+})
+export class OktaLoginComponent implements OnInit {
+
+  oktaSignin: any;
+
+  constructor(private oktaAuthService: OktaAuthService) {
+
+
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    })
+
+   }
+
+  ngOnInit(): void {
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({
+      el: '#okta-sign-in-widget'},
+      (response) => {
+        if (response.status === 'SUCCESS') {
+          this.oktaAuthService.signInWithRedirect();
+        }
+      },
+      (error) => {
+        throw error;
+      }
+    );
+  }
+
+}
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oaytyc4ch0SaCeMc5d6',
+        issuer: 'https://dev-90094617.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
